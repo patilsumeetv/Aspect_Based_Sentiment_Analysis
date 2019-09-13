5c0d682e711df03d5c10ea88a25ef39158696685
Sumeet Patil <patil.sumeet.v@gmail.com>
2019-09-13 15:10:14 +0530

    Word embedding matrix added

--- models/word2vec.py
+++ models/word2vec.py
@@ -1,4 +1,12 @@
 import tensorflow as tf
 
 def build_word2vec():
+    # pivot/input words and target/output words
+    x = tf.placeholder(tf.int32, shape = [None,], name = "x_pivot_idxs")
+    y = tf.placeholder(tf.int32, shape = [None,], name = "y_target_idxs")
+
+    # Word Embedding Matrix
+    embedding = tf.Variable(tf.random_uniform([vocab_size, embedding_size],
+                            minval=-1.0, maxval=1.0), name = "word_embedding")
+
     return
